af66882ffb5c113931b48227a89bbd8cd7383276
Ricardo J. Ferrari <ricjferrari@gmail.com>
2022-12-16 19:34:36 -0300

    style(app): format all files

--- apps/person/migrations/0001_initial.py
+++ apps/person/migrations/0001_initial.py
@@ -7,21 +7,53 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
             name='Person',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('first_name', models.CharField(max_length=100, verbose_name='nome')),
-                ('last_name', models.CharField(max_length=100, verbose_name='sobrenome')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    ),
+                ),
+                (
+                    'first_name',
+                    models.CharField(max_length=100, verbose_name='nome'),
+                ),
+                (
+                    'last_name',
+                    models.CharField(max_length=100, verbose_name='sobrenome'),
+                ),
                 ('age', models.SmallIntegerField(verbose_name='idade')),
-                ('birthday', models.DateField(verbose_name='data de nascimento')),
-                ('email', models.EmailField(max_length=255, verbose_name='e-mail')),
-                ('nickname', models.CharField(blank=True, max_length=100, null=True, verbose_name='apelido')),
-                ('note', models.TextField(blank=True, null=True, verbose_name='observação')),
+                (
+                    'birthday',
+                    models.DateField(verbose_name='data de nascimento'),
+                ),
+                (
+                    'email',
+                    models.EmailField(max_length=255, verbose_name='e-mail'),
+                ),
+                (
+                    'nickname',
+                    models.CharField(
+                        blank=True,
+                        max_length=100,
+                        null=True,
+                        verbose_name='apelido',
+                    ),
+                ),
+                (
+                    'note',
+                    models.TextField(
+                        blank=True, null=True, verbose_name='observação'
+                    ),
+                ),
             ],
             options={
                 'verbose_name': 'Pessoa',
