c475f4732366d97e7b3ae45d7b19959ec9aeaba9
Ricardo J. Ferrari <ricjferrari@gmail.com>
2022-12-17 05:19:27 -0300

    fix(app:person): update age field to only accept integers

--- apps/person/models.py
+++ apps/person/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=100, verbose_name='nome')
     last_name = models.CharField(max_length=100, verbose_name='sobrenome')
-    age = models.SmallIntegerField(verbose_name='idade')
+    age = models.PositiveSmallIntegerField(verbose_name='idade')
     birthday = models.DateField(verbose_name='data de nascimento')
     email = models.EmailField(max_length=255, verbose_name='e-mail')
     nickname = models.CharField(
@@ -12,7 +12,7 @@ class Person(models.Model):
     )
     note = models.TextField(null=True, blank=True, verbose_name='observação')
 
-    def get_full_name(self):
+    def get_full_name(self) -> str:
         full_name = (
             f'{self.first_name.title()} {self.last_name.title()}'.strip()
         )
